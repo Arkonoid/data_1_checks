a1f7353ca7f6a27fb23cdb4b0922934f92e46994
Arkonoid <tyler@tylermc.me>
2022-11-17 17:29:43 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print('hello world!')
+list1 = [1, 'a', 'here is a list', 25]
+print(list1[1])
+dictionary1 = {1: "Light", 2: "Heavy"}
+print(dictionary1[1])
+tuple1 = ("One", "Two", "Three")
+print(tuple1[2])
